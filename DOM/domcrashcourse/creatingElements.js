f27ae0d0da0f3975371f6595931af7e1fac39529
Sabina Decowska <sabina.decowska@gmail.com>
2018-08-13 10:31:36 +0200

    creating elements

---
+++ DOM/domcrashcourse/creatingElements.js
@@ -0,0 +1,24 @@
+// CREATE ELEMENT
+
+var newDiv = document.createElement('div');
+// adding class
+newDiv.className = "hello";
+// adding id
+newDiv.id = "goodbye";
+// adding attribute and its value
+newDiv.setAttribute("title", "Hello Div");
+
+// creating text node
+var newDivText = document.createTextNode("Hello World");
+
+// adding text to div
+newDiv.appendChild(newDivText);
+
+// inserting div into DOM
+var container = document.querySelector("header .container");
+var h1 = document.querySelector("header h1");
+container.insertBefore(newDiv, h1);
+console.log(newDiv);
+
+// styling
+newDiv.style.color = "orange";
